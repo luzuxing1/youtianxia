8bad5d9dbbf63103f4787fd009d48feb51fd66b8
lixiaopengwww <13297929697lxp>
2018-06-06 20:07:16 +0800

    v9 lxp 6/6 增加usermapper的登录方法

--- src/main/java/com/youtx/rent/dao/UserMapper.java
+++ src/main/java/com/youtx/rent/dao/UserMapper.java
@@ -1,7 +1,9 @@
 package com.youtx.rent.dao;
 
 import com.youtx.rent.entity.User;
+import org.mybatis.spring.annotation.MapperScan;
 
+@MapperScan
 public interface UserMapper {
     int deleteByPrimaryKey(Integer userId);
 
@@ -14,4 +16,9 @@ public interface UserMapper {
     int updateByPrimaryKeySelective(User record);
 
     int updateByPrimaryKey(User record);
+
+    /**
+     * 登录方法
+     */
+    User checkPhoneAndEmail(String phoneOrEmail) throws Exception;
 }
