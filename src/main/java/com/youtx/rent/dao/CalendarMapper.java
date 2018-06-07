267e4c0ceb9431c2e1c8f628401e13e79feed256
luzuxing1 <554102005@qq.com>
2018-06-07 18:38:36 +0800

    commit_order.htl相关controller增加，Calendar及PriceMapper.xml和DAo接口增加

--- src/main/java/com/youtx/rent/dao/CalendarMapper.java
+++ src/main/java/com/youtx/rent/dao/CalendarMapper.java
@@ -1,6 +1,9 @@
 package com.youtx.rent.dao;
 
 import com.youtx.rent.entity.Calendar;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface CalendarMapper {
     int deleteByPrimaryKey(Integer calendarId);
@@ -14,4 +17,6 @@ public interface CalendarMapper {
     int updateByPrimaryKeySelective(Calendar record);
 
     int updateByPrimaryKey(Calendar record);
+
+    List<Calendar> selectPriceByDate(@Param ( "begintime" ) String begintime, @Param ( "endtime" )String endtime);
 }
