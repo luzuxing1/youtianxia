2d9c53dfd7419a284340e2f4acf3a02426f57f24
fei <326720837@qq.com>
2018-06-09 11:33:09 +0800

    V11 租客订单页面 Controller更新 孙志飞

--- src/main/java/com/youtx/rent/dao/LodgerOrderMapper.java
+++ src/main/java/com/youtx/rent/dao/LodgerOrderMapper.java
@@ -15,12 +15,15 @@ public interface LodgerOrderMapper {
     LodgerOrder selectByPrimaryKey(Integer lodgerOrderId);
 
     List<LodgerOrder> selectByUserId(Integer userId);
-
     List<LodgerOrder> selectByStatus(@Param("userId") Integer userId, @Param("status") String status);
+    List<LodgerOrder> selectByOrderNum(@Param("userId") Integer userId,@Param("orderNum") String orderNum);
+    LodgerOrder selectByScheduleId(@Param("userId") Integer userId,@Param("scheduleId") Integer scheduleId);
+
 
     int CountAllOrder(Integer userId);
     int CountStatusOrder(@Param("userId") Integer userId,@Param("status") String orderStatus);
     int updateByPrimaryKeySelective(LodgerOrder record);
+    void updateStatusById(@Param("lodgerOrderId") Integer lodgerOrderId,@Param("orderStatus") String status);
 
     int updateByPrimaryKey(LodgerOrder record);
 }
--- src/main/java/com/youtx/rent/dao/ScheduleMapper.java
+++ src/main/java/com/youtx/rent/dao/ScheduleMapper.java
@@ -2,6 +2,8 @@ package com.youtx.rent.dao;
 
 import com.youtx.rent.entity.Schedule;
 
+import java.util.List;
+
 public interface ScheduleMapper {
     int deleteByPrimaryKey(Integer scheduleId);
 
@@ -10,7 +12,7 @@ public interface ScheduleMapper {
     int insertSelective(Schedule record);
 
     Schedule selectByPrimaryKey(Integer scheduleId);
-
+    List<Integer> selectByTime(String time);
     int updateByPrimaryKeySelective(Schedule record);
 
     int updateByPrimaryKey(Schedule record);
