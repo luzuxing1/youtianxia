5bf61f68f71a4ee6d02d31ec57eefefa5fdcc501
luzuxing1 <554102005@qq.com>
2018-06-14 16:02:56 +0800

    V37 定时任务逻辑增加 方法增加 lzx

--- src/main/java/com/youtx/rent/task/service/AutoTaskService.java
+++ src/main/java/com/youtx/rent/task/service/AutoTaskService.java
@@ -1,15 +1,121 @@
 package com.youtx.rent.task.service;
 
+import com.youtx.rent.dao.CalendarMapper;
+import com.youtx.rent.dao.PriceMapper;
+import com.youtx.rent.dao.RoomMapper;
+import com.youtx.rent.entity.Price;
+import com.youtx.rent.entity.Room;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 @Service
 public class AutoTaskService {
+    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+    Calendar cal = Calendar.getInstance ();
+    com.youtx.rent.entity.Calendar calendar = null;
+    Room room = new Room ();
+
+    @Resource
+    private RoomMapper roomMapper;
+    @Resource
+    private CalendarMapper calendarMapper;
+    @Resource
+    private PriceMapper priceMapper;
+
+    public List<Integer> selectByState(Integer state){
+        return roomMapper.selectByState (state);
+    }
+
+    public String dateToString(Date date){
+        return simpleDateFormat.format (date);
+    }
+
+    public Date stringToDate(String date){
+        try {
+            return simpleDateFormat.parse (date);
+        } catch (ParseException e) {
+            e.printStackTrace ();
+        }
+        return null;
+    }
+
+    public String nextMonthend(){
+        Calendar cal = Calendar.getInstance ();
+        cal.add (Calendar.MONTH,+2);
+        cal.set (Calendar.DAY_OF_MONTH,0);
+        return dateToString(cal.getTime ());
+    }
+
+    public Date nextDay(Date date){
+        cal.setTime (date);
+        cal.add(Calendar.DATE,1);
+        return cal.getTime ();
+    }
+
+    public String dateToWeek(Date date){
+        String[] weekDays = { "星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六" };
+        cal.setTime (date);
+        int w = cal.get(Calendar.DAY_OF_WEEK) - 1;
+        if (w < 0)
+            w = 0;
+        return weekDays[w];
+    }
     /**
      * 执行定时任务逻辑
      */
     public void autoInterface(){
-        System.out.println ("the task of every time ,Now Time："+new Date ());
+        //查询已经发布的所有房间roomId
+        List<Integer> roomIds = selectByState (0);
+        if(roomIds != null){
+            for (Integer roomId : roomIds) {
+                Price roomPrice = priceMapper.selectByRoomId (roomId);
+                if(roomPrice!=null){
+                    List<Date> dates = calendarMapper.selectDateByRoomId (roomId);
+                    if(dates.size ()!=0){
+                        if(dateToString(dates.get (0)).compareTo (dateToString(new Date ()))>=0){
+                            Integer dayPrice = roomPrice.getPriceDay ();
+                            Integer weekPrice = roomPrice.getPriceWeek ();
+                            Date currentDate = dates.get (0);
+                            while (dateToString(currentDate).compareTo (nextMonthend())<0){
+                                calendar = new com.youtx.rent.entity.Calendar ();
+                                currentDate = nextDay (currentDate);
+                                calendar.setCalendarDate (currentDate);
+                                if(dateToWeek (currentDate).equals ("星期六")||dateToWeek (currentDate).equals ("星期日")){
+                                    calendar.setCalendarPrice (weekPrice);
+                                }else {
+                                    calendar.setCalendarPrice (dayPrice);
+                                }
+                                room.setRoomId (roomId);
+                                calendar.setRoom (room);
+                                calendarMapper.insert (calendar);
+                            }
+                        }
+                    }else {
+                        Integer dayPrice = roomPrice.getPriceDay ();
+                        Integer weekPrice = roomPrice.getPriceWeek ();
+                        Date currentDate = stringToDate (dateToString (new Date ()));
+                        while(dateToString (currentDate).compareTo (nextMonthend())<0){
+                            calendar = new com.youtx.rent.entity.Calendar ();
+                            currentDate = nextDay (currentDate);
+                            calendar.setCalendarDate (currentDate);
+                            if(dateToWeek (currentDate).equals ("星期日")||dateToWeek (currentDate).equals ("星期六")){
+                                calendar.setCalendarPrice (weekPrice);
+                            }else {
+                                calendar.setCalendarPrice (dayPrice);
+                            }
+                            room.setRoomId (roomId);
+                            calendar.setRoom (room);
+                            calendarMapper.insert (calendar);
+                        }
+                    }
+                }
+            }
+        }
     }
 }
--- src/main/java/com/youtx/rent/utils/Test.java
+++ src/main/java/com/youtx/rent/utils/Test.java
@@ -1,4 +1,43 @@
 package com.youtx.rent.utils;
 
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class Test {
+    static SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+    public static String dateFormat(Date date){
+        return simpleDateFormat.format (date);
+    }
+    public static Date stringToDate(String date){
+        try {
+            Date parse = simpleDateFormat.parse ( date );
+            return parse;
+        } catch (ParseException e) {
+            e.printStackTrace ();
+        }
+        return null;
+    }
+    public static String nextMonthend(String date){
+        Calendar cal = Calendar.getInstance ();
+        //cal.setTime (stringToDate(date));
+
+        cal.add (Calendar.MONTH,+2);
+        cal.set (Calendar.DAY_OF_MONTH,0);
+        return dateFormat(cal.getTime ());
+    }
+
+    public static String everyDay(String date){
+        Calendar cal = Calendar.getInstance ();
+        cal.setTime (stringToDate(date));
+        cal.add(Calendar.DATE,1);
+        return dateFormat(cal.getTime ());
+    }
+
+    public static void main(String[] args) {
+        String s = everyDay ("2018-2-28");
+        System.out.println (s);
+    }
 }
